6c9e07807f2752f5f3dacb8ebc0dfebe4080d82a
cilliandevops <cilliandevops@gmail.com>
2024-08-15 23:03:40 +0800

    update server

---
+++ server-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/cilliandevops/kops
+
+go 1.22.5
---
+++ server-go/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
