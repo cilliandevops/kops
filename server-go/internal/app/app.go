f0aa31431292c752f94856571cbe310b7e605e1d
cilliandevops <cilliandevops@gmail.com>
2024-08-23 22:55:14 +0800

    feta: update service

--- server-go/internal/app/app.go
+++ server-go/internal/app/app.go
@@ -10,20 +10,22 @@ type App struct {
 	Engine            *gin.Engine
 	DeploymentService *services.DeploymentService
 	PodService        *services.PodService
+	ServiceService    *services.ServiceService
 }
 
 // NewApp creates a new instance of the App with the provided DeploymentService.
-func NewApp(deploymentService *services.DeploymentService, podService *services.PodService) *App {
+func NewApp(deploymentService *services.DeploymentService, podService *services.PodService, serviceService *services.ServiceService) *App {
 	// 初始化 Gin 引擎
 	router := gin.Default()
 
 	// 注册 API 路由，并将 DeploymentService 传递给路由
-	routes.RegisterK8sRoutes(router, deploymentService, podService)
+	routes.RegisterK8sRoutes(router, deploymentService, podService, serviceService)
 
 	return &App{
 		Engine:            router,
 		DeploymentService: deploymentService,
 		PodService:        podService,
+		ServiceService:    serviceService,
 	}
 }
 
